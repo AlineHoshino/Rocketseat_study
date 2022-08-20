ed6e8e2c2baf3b1ae652f6737793fa3adddad080
ALINE <hoshinoaline@yahoo.com.br>
2022-08-20 18:34:51 -0300

    feat: add ES

---
+++ ES6/main.js
@@ -0,0 +1,11 @@
+// import {text, message} from './utils.js'
+
+// console.log(text, message)
+
+// import {sum as soma } from './utils.js'
+
+// console.log(soma(1,2))
+
+import * as calculator from './utils.js'
+
+console.log(calculator.divide(10,2))
---
+++ ES6/utils.js
@@ -0,0 +1,21 @@
+// const message = "Teste"
+// const text ='outra mensagem'
+
+// // export default message
+
+// // export default function(){
+// //     return 'alo'
+// // }
+
+// export { text, message}
+
+// export function sum(a,b){
+//     return a +b
+// }
+
+export const sum =(a,b) => a + b
+export const mult =(a,b) => a * b
+export const subtract =(a,b) => a - b
+export const divide =(a,b) => a / b
+
+
